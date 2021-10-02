c4b052e3023b6f347f018716316543f86db1f4d8
Takumi <nase49946@gmail.com>
2021-10-02 17:35:43 +0900

    ハンバーガーメニューコンポーネント作成

---
+++ components/layouts/Hamburger.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="l-hamburger">
+    <span class="l-hamburger__menu"></span>
+    <span class="l-hamburger__menu"></span>
+    <span class="l-hamburger__menu"></span>
+  </button>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .l-hamburger {
+    display: none;
+    width: 32px;
+    height: 28px;
+    cursor: pointer;
+    @include responsive(md) {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-direction: column;
+    }
+
+    .l-hamburger__menu {
+      display: block;
+      width: 100%;
+      height: 3px;
+      background-color: $primaryDark;
+    }
+  }
+</style>
