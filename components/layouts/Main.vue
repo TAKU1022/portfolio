fb617aa60c91b884d0a465a6e7590f369ee508d4
Takumi <nase49946@gmail.com>
2021-10-08 23:08:38 +0900

    skillsセクション作成

--- components/layouts/Main.vue
+++ components/layouts/Main.vue
@@ -3,15 +3,17 @@
     <TopSection />
     <PointsSection />
     <ProductsSection />
+    <SkillsSection />
   </main>
 </template>
 
 <script>
   import PointsSection from '../sections/AttitudeSection.vue';
   import ProductsSection from '../sections/ProductsSection.vue';
+  import SkillsSection from '../sections/SkillsSection.vue';
   import TopSection from '../sections/TopSection.vue';
 
   export default {
-    components: { TopSection, PointsSection, ProductsSection },
+    components: { TopSection, PointsSection, ProductsSection, SkillsSection },
   };
 </script>
